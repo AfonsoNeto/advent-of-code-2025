00ea8997084f252521ee24ba181452e73d76abed
Afonso Neto <afonso.pontesneto@gmail.com>
2025-12-04 16:33:38 +0000

    [#day02 - part01] Implements solution

---
+++ day02/part01/solution.rb
@@ -0,0 +1,36 @@
+file_content = File.read('input.txt')
+invalid_ids = []
+
+def invalid_id?(id)
+  individual_characters = id.to_s.split('')
+
+  # An invalid ID is a sequence of number repeated twice, so it can only be invalid for even num of characters
+  return false if individual_characters.size.odd?
+
+  # If by removing the duplicate characters (.uniq) we get just one char, than it is an invlida ID
+  if individual_characters.uniq.size.eql?(1)
+    puts "invalid: #{id}"
+    return true
+  end
+
+  # Split the id equally from start to the half of it and from the half of it to the end
+  first_part = id.to_s.slice(...(id.to_s.size / 2))
+  second_part = id.to_s.slice((id.to_s.size/2)..)
+
+  # If both splits are equal, than we have an invalid ID
+  if first_part.eql?(second_part)
+    puts "invalid: #{id}"
+    return true
+  end
+
+  false
+end
+
+file_content.split(',').each do |range_text|
+  range = range_text.split('-')
+  range_array = (range.first.to_i..range.last.to_i).to_a
+
+  invalid_ids << range_array.select { invalid_id?(_1) }
+end
+
+puts "The sum of all invalid IDs is: #{invalid_ids.flatten.sum}"
